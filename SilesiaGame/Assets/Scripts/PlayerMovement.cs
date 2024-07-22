d0cb6e650f8f6c826ef1dd555e13b3d2dca49caf
ljchandler21 <loganjchandler@gmail.com>
2024-07-22 16:17:01 +0200

    Camera and Player movement v1.2
    First set of completed, working code for both player and camera movement.
    - player movement works by adding force, must have a drag value to counteract unlimited acceleration
    - whatever surface is our "floor" has to have a layer attribution called whatIsGround and that has to be fed as a public input to the parameters for the player movement script. Player height for a default pill is 2
    - ground detection functions off of a raycast that looks how far to the ground the player object is.
    - Camera rotation and movement code is attached to PlayerCam, but with a script in CamHolder that just places the camera onto the player using the transform value from CameraPos within the player object.

--- SilesiaGame/Assets/Scripts/PlayerMovement.cs
+++ SilesiaGame/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,12 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
 
+    public float groundDrag; 
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded; 
 
     public Transform orientation;
 
@@ -24,9 +30,42 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
     }
 
+    // FixedUpdate handles all physics-related movement
+    private void FixedUpdate()
+    {
+        MovePlayer(); 
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        
+        // ground check 
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); 
+
+
+        MyInput();
+
+        // handle drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else rb.drag = 0; 
+
+
+
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical"); 
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force); 
     }
 }
