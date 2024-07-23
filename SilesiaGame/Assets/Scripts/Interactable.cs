c165a01a8f394d9feaff9a3ad94b171a72719a03
gonerik <163130399+gonerik@users.noreply.github.com>
2024-07-23 15:46:52 +0200

    OnHover add

--- SilesiaGame/Assets/Scripts/Interactable.cs
+++ SilesiaGame/Assets/Scripts/Interactable.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Interactable : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
     private int id;
     private static int maxID = 1;
@@ -12,6 +12,7 @@ public abstract class Interactable : MonoBehaviour
 
     //datadump of the object. Here we store the serialized info.
     [SerializeField] private InteractableSO data;
+    [SerializeField] private GameObject outline;
 
 
     private void Start()
@@ -20,27 +21,28 @@ public abstract class Interactable : MonoBehaviour
         maxID += 1;
         listOfAllObjects.Add(this);
         ableToUse = data.basicState;
+        PlayerMovementInteract.Instance.OnSelectedArtefactChanged += Instance_OnSelectedArtefactChanged;
+        Debug.Log("Started");
+    }
+
+    private void Instance_OnSelectedArtefactChanged(object sender, PlayerMovementInteract.OnSelectedArtefactChangedEventArgs e)
+    {
+        if (this == e.selectedArtefact)
+        {
+            setOutlineON();
+        }
+        else
+        {
+            setOutlineOFF();
+        }
     }
 
     // method called on interacting with an object
     //this should be completly rewriten once the inpput system will be made
     //see the OnInteraction State Diagram (lucidchart)
-    public void OnInteraction()
-    {
-        if (ableToUse)
-        {
-            Debug.Log(getLine(0));
-            GetComponent<ICameraMovementType>().cameraMoveIn();
-        }
-
-    }
 
     // method called on hovering over the object
-    public void OnHover()
-    {
-        Debug.Log(gameObject.name + " was hovered");
-    }
-    
+
     public string getLine(int lineNumber)
     {
         return GetComponent<InteractableSO>().text.text.Split('\n')[lineNumber];
@@ -49,5 +51,17 @@ public abstract class Interactable : MonoBehaviour
     public virtual void Interact()
     {
     }
+    
 
+    public void setOutlineON()
+    {
+        this.outline.SetActive(true);
+
+    }
+    public void setOutlineOFF()
+    {
+        
+        this.outline.SetActive(false);
+        
+    }
 }
--- SilesiaGame/Assets/Scripts/Player/PlayerMovementInteract.cs
+++ SilesiaGame/Assets/Scripts/Player/PlayerMovementInteract.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementInteract : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed;
@@ -20,37 +21,47 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
 
     [SerializeField] private Camera cam;
-    [SerializeField]
     private PlayerInput input;
-    [SerializeField] private float distance = 3f;
+    [SerializeField] private float distance = 8f;
     [SerializeField] private LayerMask mask;
     private PlayerUI playerUI;
 
-    private Interactable facedInteractable;
+    public static PlayerMovementInteract Instance { get; set; }
+
+    public static Interactable selectedInteractable;
+    public event EventHandler<OnSelectedArtefactChangedEventArgs> OnSelectedArtefactChanged;
+
+    public class OnSelectedArtefactChangedEventArgs : EventArgs
+    {
+        public Interactable selectedArtefact;
+    }
     // Start is called before the first frame update
     //input = PlayerInput, made with InputSystem package
     // += stands for listening to the event triggered by PlayerInput,
     //once it is triggered the GameInput_OnInteraction is executed
     //playerUI - test thing, replace with onHover system later
+    void Awake()
+    {
+        if(Instance != null)
+            Debug.LogError("There are two players???");
+        Instance = this;
+    }
     void Start()
     {
+        playerUI = GetComponent<PlayerUI>();
+        input = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         input.OnInteraction += GameInput_OnInteraction;
         playerUI = GetComponent<PlayerUI>();
     }
+    
 
     private void GameInput_OnInteraction(object sender, EventArgs e)
     {
-        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
-        RaycastHit hitInfo;
-        if (Physics.Raycast(ray, out hitInfo, distance, mask))
+        if (selectedInteractable != null)
         {
-            facedInteractable = hitInfo.collider.GetComponent<Interactable>();
-            if (facedInteractable != null)
-            {
-                facedInteractable.Interact();
-            }
+            selectedInteractable.Interact();
         }
     }
 
@@ -60,26 +71,40 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer();
     }
 
+    private Interactable getSelectedInteractable()
+    {
+        return selectedInteractable;
+    }
+
 
     // Update is called once per frame
     //lines 74-83 for hovering
     void Update()
     {
         // ground check 
-
-        MyInput();
         playerUI.UpdateText(String.Empty);
+        MyInput();
         // handle drag
         rb.drag = groundDrag;
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
         RaycastHit hitInfo;
         if (Physics.Raycast(ray, out hitInfo, distance, mask))
         {
-            facedInteractable = hitInfo.collider.GetComponent<Interactable>();
-            if (facedInteractable != null)
+            var facedInteractable = hitInfo.collider.GetComponent<Interactable>();
+            if (facedInteractable != null && Vector3.Distance(facedInteractable.transform.position, rb.position)<100)
             {
                 playerUI.UpdateText("Press E to interact");
+                if (facedInteractable != selectedInteractable)
+                {
+                    SetSelectedArtefact(facedInteractable);
+                }
             }
+
+
+        }
+        else
+        {
+            SetSelectedArtefact(null);
         }
 
 
@@ -97,6 +122,15 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force); 
+    }
+
+    private void SetSelectedArtefact(Interactable artefact)
+    {
+        selectedInteractable = artefact;
+        OnSelectedArtefactChanged?.Invoke(this, new OnSelectedArtefactChangedEventArgs()
+        {
+            selectedArtefact = artefact
+        });
     }
     
 }
