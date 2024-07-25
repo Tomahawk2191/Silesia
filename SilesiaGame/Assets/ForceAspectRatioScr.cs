3ae8e0c91e225a124a67974ad3058ff11c28654c
14tp000 <118660337+14tp000@users.noreply.github.com>
2024-07-25 09:26:26 +0200

    aspect forcing is now fixed

--- SilesiaGame/Assets/ForceAspectRatioScr.cs
+++ SilesiaGame/Assets/ForceAspectRatioScr.cs
@@ -6,22 +6,12 @@ using UnityEngine.UI;
 public class ForceAspectRatioScr : MonoBehaviour
 {
 
-    [SerializeField] int width = 1920;
-    [SerializeField] int height = 1080;
-    //[SerializeField] RenderTexture outlineOut;
-    [SerializeField] RawImage ppFilter;
+    [SerializeField] float aspectX = 16;
+    [SerializeField] float aspectY = 9;
 
-    [SerializeField] Material pProcess;
     void Start()
     {
-        //outlineOut.width = Screen.width;
-        //outlineOut.height = Screen.height;
-        //ppFilter.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
-        //outlineCam.fieldOfView = Camera.main.fieldOfView;
-
-        
-
-        float targetAspect = (float)width / (float)height;
+        float targetAspect = aspectX / aspectY;
 
         float windowAspect = (float)Screen.width / (float)Screen.height;
 
@@ -29,23 +19,19 @@ public class ForceAspectRatioScr : MonoBehaviour
 
         if (scaleHeight < 1.0f)
         {
-            pProcess.SetVector("offset", new Vector2(0, 0));
-            ppFilter.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height * scaleHeight);
             Rect rect = Camera.main.rect;
 
             rect.width = 1.0f;
             rect.height = scaleHeight;
             rect.x = 0;
-            rect.y = 0;
-            //rect.y = (1.0f - scaleHeight) / 2.0f;
+            //rect.y = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
 
             Camera.main.rect = rect;
         }
         else
         {
             float scaleWidth = 1.0f / scaleHeight;
-            pProcess.SetVector("offset", new Vector2(scaleWidth, 0));
-            ppFilter.rectTransform.sizeDelta = new Vector2(Screen.width* scaleWidth, Screen.height);
 
             Rect rect = Camera.main.rect;
 
@@ -56,9 +42,7 @@ public class ForceAspectRatioScr : MonoBehaviour
 
             Camera.main.rect = rect;
         }
-
-        ppFilter.rectTransform.position = new Vector2(Screen.width / 2, Screen.height / 2);
-        
+      
         
     }
 }
