8e41e57c5d426e0d7bb92fc9962649cca886707b
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2024-08-05 10:59:54 +0200

    Mid-Video Fix

--- SilesiaGame/Assets/Scripts/MainMenu.cs
+++ SilesiaGame/Assets/Scripts/MainMenu.cs
@@ -1,10 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Start()
+    { 
+        VideoPlayer videoPlayer =
+        GameObject.Find("Canvas").transform.Find("MainMenuLoop").GetComponent<VideoPlayer>();
+        videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,"MainMenuLoop.mp4");
+        videoPlayer.Play();
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
