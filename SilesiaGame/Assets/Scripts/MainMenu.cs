4b0ba8a7c094f330d1c48ab5e18fcb67fe9fe140
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2024-08-05 13:48:18 +0200

    Main Menu changed mp4 to jpgs changing to accomodate webGL

--- SilesiaGame/Assets/Scripts/MainMenu.cs
+++ SilesiaGame/Assets/Scripts/MainMenu.cs
@@ -7,14 +7,6 @@ using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
-    private void Start()
-    { 
-        VideoPlayer videoPlayer =
-        GameObject.Find("Canvas").transform.Find("MainMenuLoop").GetComponent<VideoPlayer>();
-        videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,"MainMenuLoop.mp4");
-        videoPlayer.Play();
-    }
-
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
