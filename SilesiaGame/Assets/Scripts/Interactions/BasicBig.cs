068654fd6291f6dafdfa59bf6d57f604949c143e
Isaac Polite <145938552+Tomahawk2191@users.noreply.github.com>
2024-08-05 13:08:08 +0200

    changed lighting to mixed, added some prefabs

--- SilesiaGame/Assets/Scripts/Interactions/BasicBig.cs
+++ SilesiaGame/Assets/Scripts/Interactions/BasicBig.cs
@@ -6,6 +6,7 @@ public class BasicBig : Interactable
 {
     private void Awake()
     {
+        cameraMovementType = new CameraForBigObjects();
     }
 
     //Interact is empty because it doesn't do anything else other than run dialogue
