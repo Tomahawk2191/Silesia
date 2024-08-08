92ea81b35ec0e5434a17cc747b60f3932f8e9aef
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2024-08-08 11:36:42 +0200

    Ending done

--- SilesiaGame/Assets/Scripts/LetterScript.cs
+++ SilesiaGame/Assets/Scripts/LetterScript.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
+
 public class LetterScript : MonoBehaviour
 {
     [SerializeField] private float waitTimeSeconds = 3f;
@@ -14,6 +16,8 @@ public class LetterScript : MonoBehaviour
     
     [SerializeField] private Material _introMaterial;
     [SerializeField] private Material _outroMaterial;
+
+    [SerializeField] private GameObject _backGround;
     // Start is called before the first frame update
     void Start()
     {
@@ -58,20 +62,46 @@ public class LetterScript : MonoBehaviour
         
         transform.DOLocalMoveY(-1.2f, 1.5f).SetEase(Ease.InOutExpo);
         yield return new WaitForSeconds(1.5f);
-        gameObject.SetActive(false);
+        _skinnedMeshRenderer.enabled = false;
         
-        
-        /*//OUTRO LETTER
-        yield return new WaitForSeconds(1.5f);
-        gameObject.SetActive(true);
+        playerInteract.unblockPlayerFromDialogue();
+        StartCoroutine(ScrollOutroLetter(introLetter, playerInteract));
+    }
+
+    IEnumerator ScrollOutroLetter(GameObject introLetter, PlayerInteract playerInteract)
+    {
+        playerInteract.blockPlayerForDialogue();
+        _skinnedMeshRenderer.enabled = true;
         _skinnedMeshRenderer.material = _outroMaterial;
         
         transform.DOLocalMoveY(1.16f, 1.5f).SetEase(Ease.InOutExpo);
+        yield return new WaitForSeconds(1.5f);
+        
+        transform.GetChild(0).GetComponent<Animator>().SetTrigger("Folded");
+        AudioManager.instance.Play("LetterFold"); 
         yield return new WaitForSeconds(1.25f);
-        */
+
+        
+        transform.DOLocalMoveZ(0.64f, 1.5f).SetEase(Ease.InCubic);
+        yield return new WaitForSeconds(1.5f);
+        
+        _renderer.SetActive(false);
+        
+        yield return new WaitForSeconds(waitTimeSeconds);
+        while (introLetter.transform.localPosition.y < 1.9f)
+        {
+            introLetter.transform.Translate(Vector3.up * Time.deltaTime * scrollSpeed/100);
+            yield return null;
+        }
+        
+        yield return new WaitForSeconds(waitTimeSeconds);
+        
+        _backGround.GetComponent<Animator>().SetTrigger("FadeOut");
+        
+        yield return new WaitForSeconds(7f);
+        
+        SceneManager.LoadSceneAsync(2);
         
         
-        playerInteract.unblockPlayerFromDialogue();
-        Debug.Log("yikes");
     }
 }
