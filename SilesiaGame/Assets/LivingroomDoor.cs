305996487c4fb3377ed9410de159185d88065170
ljchandler21 <loganjchandler@gmail.com>
2024-08-09 15:45:20 +0200

    bedroom and living room door prefabs

---
+++ SilesiaGame/Assets/LivingroomDoor.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class LivingroomDoor : MonoBehaviour
+{
+    private static int interactions = 0;
+    private static LivingroomDoor Instance { get; set; }
+
+    private static Animator _animator;
+    private static bool bedDoorOpen = false;
+
+    private static Vector3 bedDoorPos;
+    private static Vector3 livingDoorPos;
+    private AudioManager audioManager;
+
+
+    private void Start()
+    {
+        audioManager = AudioManager.instance;
+        bedDoorPos = audioManager.GetBedDoorPos();
+        livingDoorPos = audioManager.GetLivingDoorPos();
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    public static void OpenDoor()
+    {
+        Debug.Log("openThedoor");
+        _animator.SetTrigger("OpenDoor");
+        //PlayDoorSound();
+        if (!bedDoorOpen)
+        {
+            AudioManager.instance.Play("DoorOpen", bedDoorPos);
+            bedDoorOpen = true;
+        }
+        AudioManager.instance.Play("DoorOpen", livingDoorPos);
+
+    }
+
+    public static void newInteraction()
+    {
+        interactions++;
+        if (interactions == 1)
+        {
+            Debug.Log("openThedoor");
+            _animator.SetTrigger("OpenDoor");
+        }
+    }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.Log("Two instances of the door");
+        }
+        else
+        {
+            Instance = this;
+            _animator = transform.parent.GetComponent<Animator>();
+            bedDoorPos = AudioManager.instance.GetBedDoorPos();
+            livingDoorPos = AudioManager.instance.GetLivingDoorPos();
+        }
+    }
+}
