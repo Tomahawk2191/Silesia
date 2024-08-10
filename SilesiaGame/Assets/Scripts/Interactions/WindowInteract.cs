9fb369f4b987fe0b207a3ca7f5327bf6a292e1ff
ben_jaccaci <ben.jaccaci@gmail.com>
2024-08-10 14:51:29 +0200

    Reapply "updating window interaction (incomplete)"
    This reverts commit 743def39af68dd678b35456e91d53b9f73ceff5f.

---
+++ SilesiaGame/Assets/Scripts/Interactions/WindowInteract.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowInteract : Interactable
+{
+    private void Awake()
+    {
+        cameraMovementType = new CameraForBigObjects();
+    }
+
+    //Interact is empty because it doesn't do anything else other than run dialogue
+    public override void Interact()
+    {
+        KitchenWindow.OpenWindow(); 
+    }
+}
