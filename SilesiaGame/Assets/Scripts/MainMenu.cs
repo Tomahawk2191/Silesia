2a13b2d5cd5342da74335b5ecd37771fa7d432c8
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2024-08-13 03:47:10 +0200

    Adding basic sfx to main menu

--- SilesiaGame/Assets/Scripts/MainMenu.cs
+++ SilesiaGame/Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
@@ -7,10 +8,13 @@ public class MainMenu : MonoBehaviour
     public Texture2D normalCursor;
     public Texture2D interactCursor;
 
+    [SerializeField] private AudioSource _wind;
+
     public void Awake()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.Auto);
+        StartCoroutine(windSounds());
     }
 
     public void OnPointerEnter()
@@ -31,5 +35,14 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator windSounds()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+            _wind.Play();
+        }
     }
 }
